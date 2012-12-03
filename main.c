81a6fb391b9bf6941f440c1d4d287bb9be5e0160
Tom Leaman <tom@tomleaman.co.uk>
2012-12-03 21:21:10 +0000

    Add buggy stuff for reading in nodes

--- main.c
+++ main.c
@@ -10,9 +10,28 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef enum {
+  NOT_STARTED,
+  RUNNING,
+  FINISHED
+} status_t; 
+
+typedef enum {
+  CP,
+  MC,
+  JN
+} node_t;
+
+typedef struct Node {
+  int id;
+  node_t type;
+  struct Node* next;
+} Node;
+
 typedef struct Competitor {
   int id;
   char* name;
+  status_t status;
   struct Competitor* next;
 } Competitor;
 
@@ -21,8 +40,44 @@ typedef struct Event {
   char* date;
   int start_hrs;
   int start_mins;
+  Node** node_list;
   Competitor** competitor_list;
 } Event;
+
+/* The following function was taken from
+ * http://stackoverflow.com/questions/314401/how-to-read-a-line-from-the-console-in-c
+ * It's probably overkill for this assignment but it works! */
+char* getline_foo() {
+  char* line = malloc(100), * linep = line;
+  size_t lenmax = 100, len = lenmax;
+  int c;
+
+  if(line == NULL)
+    return NULL;
+
+  for(;;) {
+    c = fgetc(stdin);
+    if(c == EOF)
+      break;
+
+    if(--len == 0) {
+      len = lenmax;
+      char * linen = realloc(linep, lenmax *= 2);
+
+      if(linen == NULL) {
+        free(linep);
+        return NULL;
+      }
+      line = linen + (line - linep);
+      linep = linen;
+    }
+
+    if((*line++ = c) == '\n')
+      break;
+  }
+  *line = '\0';
+  return linep;
+}
 
 void print_menu() {
   printf("\n");
@@ -52,7 +107,7 @@ void read_event(Event* event, char* filename) {
   char* delim = ":";
   FILE* fp = fopen(filename, "r");
 
-  fgets(line, 80, fp);
+  fgets(line, 80, fp); // Now this is borked somehow
   event->title = strdup(line);
   fgets(line, 80, fp);
   event->date = strdup(line);
@@ -61,6 +116,49 @@ void read_event(Event* event, char* filename) {
   event->start_hrs = atoi(token);
   token = strtok(NULL, delim);
   event->start_mins = atoi(token);
+
+  fclose(fp);
+}
+
+void read_nodes(Event* event, char* filename) {
+  char line[80];
+  char* token;
+  char* delim = " ";
+  FILE* fp = fopen(filename, "r");
+  Node* node;
+  Node* prev;
+
+  fgets(line, 80, fp);
+  node = malloc(sizeof(Node));
+  token = strtok(line, delim);
+  node->id = atoi(token);
+  token = strtok(NULL, delim);
+  if (strncmp(token, "CP", 2) == 0) {
+    node->type = CP;
+  } else if (strncmp(token, "MC", 2) == 0) {
+    node->type = MC;
+  } else {
+    node->type = JN;
+  }
+  node->next = NULL;
+  event->node_list = &node;
+  prev = node;
+  while (fgets(line, 80, fp) != NULL) {
+    node = NULL;
+    node = malloc(sizeof(Node));
+    token = strtok(line, delim);
+    node->id = atoi(token);
+    token = strtok(NULL, delim);
+    if (strncmp(token, "CP", 2) == 0) {
+      node->type = CP;
+    } else if (strncmp(token, "MC", 2) == 0) {
+      node->type = MC;
+    } else {
+      node->type = JN;
+    }
+    node->next = NULL;
+    prev->next = node;
+  }
 
   fclose(fp);
 }
@@ -74,7 +172,15 @@ void locate_competitor(Event* event) {
   competitor = (*event->competitor_list);
   while (competitor != NULL) {
     if (competitor->id == input) {
-      printf("%d: %s STATUS\n", competitor->id, competitor->name); // TODO Plug-in the actual status
+      printf("%d: %s ", competitor->id, competitor->name);
+      // TODO if the competitor is running, it should show where he/she is on the course
+      if (competitor->status == NOT_STARTED) {
+        printf("Not started\n");
+      } else if (competitor->status == RUNNING) {
+        printf("Running\n");
+      } else {
+        printf("Finished\n");
+      }
       return;
     }
     competitor = competitor->next;
@@ -83,16 +189,21 @@ void locate_competitor(Event* event) {
 
 int main(int argc, char* argv[]) {
   int input;
-  char filename[80];
+  char* filename;
   Event* event;
 
   /* Init data structures and whatnot */
   event = malloc(sizeof(Event));
+  event->node_list = NULL;
   event->competitor_list = NULL;
 
   printf("Please enter the name of the file containing event data: ");
-  scanf("%79[a-zA-Z0-9._/]", filename);
+  filename = getline_foo();
   read_event(event, filename);
+
+  printf("Please enter the name of the file containing node data: ");
+  filename = getline_foo();
+  read_nodes(event, filename);
 
   /* The main program loop, shows the menu and prompts for input */
   input = -1;
