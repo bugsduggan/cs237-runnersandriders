bd3984df1e0d5c2a2ca41bf8315dd51af7a5dd3c
Tom Leaman <tom@tomleaman.co.uk>
2012-12-05 17:47:30 +0000

    Add test for pointer shizzle

--- vectortest.c
+++ vectortest.c
@@ -1,8 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "vector.h"
 
-void test_add(Vector* vector) {
+char *strdup(const char *str)
+{
+  int n = strlen(str) + 1;
+  char *dup = malloc(n);
+  if(dup) {
+    strcpy(dup, str);
+  }
+  return dup;
+}
+
+void test_add_simple(Vector* vector) {
   int i = 0;
   for (i = 1; i < 11; i++) {
     printf("Adding: %d ", i);
@@ -11,7 +22,7 @@ void test_add(Vector* vector) {
   }
 }
 
-void test_remove(Vector* vector) {
+void test_remove_simple(Vector* vector) {
   int ret_val;
   printf("Removing first elem: ");
   Vector_remove(vector, 0, &ret_val);
@@ -21,14 +32,14 @@ void test_remove(Vector* vector) {
   printf("%d Size: %d Capacity: %d\n", ret_val, vector->size, vector->capacity);
 }
 
-void test_get(Vector* vector) {
+void test_get_simple(Vector* vector) {
   int ret_val;
   printf("Getting first elem: ");
   Vector_get(vector, 0, &ret_val);
   printf("%d Size: %d Capacity: %d\n", ret_val, vector->size, vector->capacity);
 }
 
-void test_out_of_bounds(Vector* vector) {
+void test_out_of_bounds_simple(Vector* vector) {
   int ret_val;
   printf("Trying vector->size: ");
   Vector_remove(vector, vector->size, &ret_val);
@@ -38,15 +49,69 @@ void test_out_of_bounds(Vector* vector) {
   printf("Didn't crash!\n");
 }
 
-int main(int argc, char* argv[]) {
+void test_simple() {
   Vector* vector = Vector_new(sizeof(int), NULL);
-  test_add(vector);
+  test_add_simple(vector);
   printf("\n");
-  test_remove(vector);
+  test_remove_simple(vector);
   printf("\n");
-  test_get(vector);
+  test_get_simple(vector);
   printf("\n");
-  test_out_of_bounds(vector);
+  test_out_of_bounds_simple(vector);
   Vector_dispose(vector);
+}
+
+void dispose_str(void* str) {
+  char* foo = *(char**)str;
+  if (foo) free(foo);
+}
+
+void test_add_complex(Vector* vector) {
+  char* str;
+  printf("Adding 'Fred': ");
+  str = strdup("Fred");
+  Vector_add(vector, &str);
+  printf("Size: %d Capacity: %d\n", vector->size, vector->capacity);
+  printf("Adding 'Wilma': ");
+  str = strdup("Wilma");
+  Vector_add(vector, &str);
+  printf("Size: %d Capacity: %d\n", vector->size, vector->capacity);
+  printf("Adding 'Barney': ");
+  str = strdup("Barney");
+  Vector_add(vector, &str);
+  printf("Size: %d Capacity: %d\n", vector->size, vector->capacity);
+}
+
+void test_remove_complex(Vector* vector) {
+  char* str;
+  printf("Removing first elem: ");
+  Vector_remove(vector, 0, &str);
+  printf("%s Size: %d Capacity: %d\n", str, vector->size, vector->capacity);
+  free(str); /* It's now my responsibility to free this */
+}
+
+void test_get_complex(Vector* vector) {
+  char* str;
+  printf("Getting first elem: ");
+  Vector_get(vector, 0, &str);
+  printf("%s Size: %d Capacity: %d\n", str, vector->size, vector->capacity);
+}
+
+void test_complex() {
+  Vector* vector = Vector_new(sizeof(char**), dispose_str);
+  test_add_complex(vector);
+  printf("\n");
+  test_remove_complex(vector);
+  printf("\n");
+  test_get_complex(vector);
+  /* no real need to test out of bounds again */
+  Vector_dispose(vector);
+}
+
+int main(int argc, char* argv[]) {
+  printf("\n\nTesting simple\n\n");
+  test_simple();
+  printf("\n\nTesting complex\n\n");
+  test_complex();
   return EXIT_SUCCESS;
 }
