46e1360d913522f2d24d53cb8db1fd9e8403f0aa
Tom Leaman <tom@tomleaman.co.uk>
2012-12-05 15:55:19 +0000

    Add generic vector

---
+++ vector.c
@@ -0,0 +1,66 @@
+/*
+ * vector
+ *
+ * Tom Leaman (thl5@aber.ac.uk)
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include "vector.h"
+
+void Vector_grow(Vector* vector) {
+  void* new_base;
+  vector->capacity *= 2;
+  new_base = malloc(vector->elem_size * vector->capacity);
+  memcpy(new_base, vector->base, vector->elem_size * vector->size);
+  free(vector->base);
+  vector->base = new_base;
+}
+
+Vector* Vector_new(int elem_size, void (*dispose_fn)(void*)) {
+  Vector* vector = malloc(sizeof(Vector));
+  vector->elem_size = elem_size;
+  vector->dispose_fn = dispose_fn;
+  vector->size = 0;
+  vector->capacity = MIN_CAPACITY;
+  vector->base = malloc(vector->elem_size * vector->capacity);
+  return vector;
+}
+
+void Vector_dispose(Vector* vector) {
+  int i = 0;
+  if (vector) {
+    if (vector->dispose_fn) {
+      for (i = 0; i < vector->size; i++) {
+        vector->dispose_fn((char*)vector->base + vector->elem_size * i);
+      }
+    }
+    if (vector->base) free(vector->base);
+    free(vector);
+  }
+}
+
+void Vector_add(Vector* vector, void* elem) {
+  void* target;
+  if (vector) {
+    if (vector->size == vector->capacity) Vector_grow(vector);
+    target = (char*) vector->base + vector->elem_size * vector->size++;
+    memcpy(target, elem, vector->elem_size);
+  }
+}
+
+void* Vector_remove(Vector* vector, int index) {
+  if (vector) {
+    if (index < vector->size)
+      return (char*)vector->base + vector->elem_size-- * index;
+  }
+  return NULL;
+}
+
+void* Vector_get(Vector* vector, int index) {
+  if (vector) {
+    if (index < vector->size)
+      return (char*)vector->base + vector->elem_size * index;
+  }
+  return NULL;
+}
---
+++ vector.h
@@ -0,0 +1,27 @@
+/*
+ * vector
+ *
+ * Tom Leaman (thl5@aber.ac.uk)
+ */
+
+#ifndef VECTOR_THL_H
+#define VECTOR_THL_H
+
+#define MIN_CAPACITY 4
+
+typedef struct Vector {
+  int elem_size;
+  void (*dispose_fn)(void*);
+  int size;
+  int capacity;
+  void* base;
+} Vector;
+
+Vector* Vector_new(int elem_size, void (*dispose_fn)(void*));
+void Vector_dispose(Vector* vector);
+
+void Vector_add(Vector* vector, void* elem);
+void* Vector_remove(Vector* vector, int index);
+void* Vector_get(Vector* vector, int index);
+
+#endif
