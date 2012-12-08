cf3e632506c53e35464f7db536f0692d2308d984
Tom Leaman <tom@tomleaman.co.uk>
2012-12-08 19:13:27 +0000

    Add comment about node order

--- track.c
+++ track.c
@@ -28,6 +28,12 @@ Vector* read_tracks(char* filename, Vector* nodes) {
   int node_id;
   int i = 0;
 
+  /*
+   * It's worth noting that at this point, the start and end nodes
+   * are set fairly arbitrarily. Once the track becomes part of a
+   * course, the nodes should be set in the correct, logical order
+   */
+
   for (i = 0; i < Vector_size(lines); i++) {
     Vector_get(lines, i, &line);
     track = malloc(sizeof(Track));
