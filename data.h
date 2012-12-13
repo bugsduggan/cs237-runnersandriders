041e279a48279813dad876d813718666e8dddee8
Tom Leaman <tom@tomleaman.co.uk>
2012-12-13 14:01:09 +0000

    Comment data.h

--- data.h
+++ data.h
@@ -71,6 +71,8 @@ typedef struct Course {
 Vector* read_courses(char* filename, Vector* nodes, Vector* tracks);
 Course* course_from_id(Vector* courses, char id);
 Track* next_track(Course* course, Track* current);
+/* grabs the next track in the course and accounts for entrants arriving so
+ * early that they confuse the track predictions from entrant_update_time */
 Track* next_track_from_node(Course* course, Track* current, Node* node);
 
 /*
@@ -78,19 +80,20 @@ Track* next_track_from_node(Course* course, Track* current, Node* node);
  */
 
 typedef enum {
-  NOT_STARTED,
-  STARTED,
-  STOPPED, /* at a medical checkpoint */
+  NOT_STARTED,    /* waiting to start */
+  STARTED,        /* running */
+  STOPPED,        /* at a medical checkpoint */
   DISQUAL_SAFETY, /* disqualified for safety */
   DISQUAL_INCORR, /* disqualified for getting lost */
-  FINISHED
+  FINISHED        /* finished */
 } entrant_status;
 
 typedef struct Entrant {
   int id;
   Course* course;
   char* name;
-  int duration;
+
+  int duration; /* the run time */
   entrant_status status;
   Time* start_time;
   /* the last timed point */
@@ -99,7 +102,7 @@ typedef struct Entrant {
   /* the assumed location */
   Node* last_node;
   Time* last_time;
-  /* the assumed track */
+  /* the current time and what track we think they're on */
   Time* curr_time;
   Track* curr_track;
 } Entrant;
@@ -107,7 +110,9 @@ typedef struct Entrant {
 Vector* read_entrants(char* filename, Vector* courses);
 Entrant* entrant_from_id(Vector* entrants, int id);
 void entrant_stats(Entrant* entrant, Time* curr_time);
+/* updates the entrant at a timed or medical checkpoint */
 void entrant_update_location(Event* event, char type, int entrant_id, int node_id);
+/* updates the running time and predicts which track they're on */
 void entrant_update_time(Event* event, Entrant* entrant);
 void entrants_sort(Event* event);
 
