dfaf37e12f4d2b2eccd781193314b6c819b8c4ba
Tom Leaman <tom@tomleaman.co.uk>
2012-12-13 13:56:59 +0000

    Add comments to main

--- main.c
+++ main.c
@@ -12,6 +12,9 @@
 #include "util.h"
 #include "data.h"
 
+/*
+ * returns a valid filename 
+ */
 char* get_filename(char* prompt) {
   char* filename;
 
@@ -26,6 +29,10 @@ char* get_filename(char* prompt) {
   return filename;
 }
 
+/*
+ * reads in data from all relevant files
+ * returns a pointer to an event object
+ */
 Event* read_data() {
   char* filename;
   Event* event;
@@ -54,6 +61,9 @@ Event* read_data() {
   return event;
 }
 
+/*
+ * print out the name and details of the event
+ */
 void display_event_header(Event* event) {
   printf("\n\n");
   printf("\t%s\n", event->title);
@@ -62,6 +72,10 @@ void display_event_header(Event* event) {
   printf("\n");
 }
 
+/*
+ * display the menu and grab an int from the user
+ * to specify what action to take
+ */
 int display_menu(Event* event) {
   char* line;
   char* token;
@@ -90,6 +104,9 @@ int display_menu(Event* event) {
   return input;
 }
 
+/*
+ * locate a particular entrant from id
+ */
 void locate_entrant(Event* event) {
   Entrant* entrant;
   int entrant_id;
@@ -105,6 +122,9 @@ void locate_entrant(Event* event) {
   free(line);
 }
 
+/*
+ * returns how many entrants have a particular status
+ */
 int count_by_status(Event* event, entrant_status status) {
   Entrant* entrant;
   int ret_val = 0;
@@ -118,9 +138,14 @@ int count_by_status(Event* event, entrant_status status) {
   return ret_val;
 }
 
+/*
+ * list entrants excluded for safety reasons
+ */
 void list_excluded_safety(Event* event) {
   Entrant* entrant;
   int i = 0;
+
+  /* make sure there's at least one */
   if (count_by_status(event, DISQUAL_SAFETY) == 0) {
     printf("\tNo entrants disqualified for safety reasons\n");
   } else {
@@ -135,9 +160,14 @@ void list_excluded_safety(Event* event) {
   }
 }
 
+/*
+ * list entrants excluded for getting lost
+ */
 void list_excluded_incorrect(Event* event) {
   Entrant* entrant;
   int i = 0;
+
+  /* make sure there's at least one */
   if (count_by_status(event, DISQUAL_INCORR) == 0) {
     printf("\tNo entrants disqualified for incorrect route\n");
   } else {
@@ -152,51 +182,68 @@ void list_excluded_incorrect(Event* event) {
   }
 }
 
+/*
+ * display all the entrants based on status/duration
+ */
 void display_results(Event* event) {
   Entrant* entrant;
   int i = 0;
 
   entrants_sort(event);
+  /* since the entrants are now sorted, we know that all entrants with a particular
+   * status will be grouped together */
+
+  /* display finished */
   if (count_by_status(event, FINISHED) > 0) {
     printf("\n\tFinished:\n");
     for (; i < Vector_size(event->entrants); i++) {
       Vector_get(event->entrants, i, &entrant);
-      if (entrant->status != FINISHED) break;
+      if (entrant->status != FINISHED) break; /* if entrant hasn't finished, skip to
+                                                 the next block of entrants */
       printf("\t\t%3d: %-50s\n", entrant->id, entrant->name);
       printf("\t\t\tCourse: %c Total time: %3d mins\n", entrant->course->id, entrant->duration);
     }
   }
+  /* display started and stopped */
   if (count_by_status(event, STARTED) + count_by_status(event, STOPPED) > 0) {
     printf("\n\tRunning:\n");
     for (; i < Vector_size(event->entrants); i++) {
       Vector_get(event->entrants, i, &entrant);
       if (entrant->status != STARTED && entrant->status != STOPPED) break;
+
       printf("\t\t%3d: %-50s\n", entrant->id, entrant->name);
       printf("\t\t\tCourse: %c Track: %2d Run time: %3d mins\n", entrant->course->id,
           entrant->curr_track->id, entrant->duration);
     }
   }
+  /* display disqualified */
   if (count_by_status(event, DISQUAL_SAFETY) + count_by_status(event, DISQUAL_INCORR) > 0) {
     printf("\n\tDisqualified:\n");
     for (; i < Vector_size(event->entrants); i++) {
       Vector_get(event->entrants, i, &entrant);
       if (entrant->status != DISQUAL_SAFETY && entrant->status != DISQUAL_INCORR) break;
+
       printf("\t\t%3d: %-50s\n", entrant->id, entrant->name);
       printf("\t\t\tCourse: %c ", entrant->course->id);
       if (entrant->status == DISQUAL_SAFETY) printf("Disqualified for safety\n");
       else printf("Disqualified for incorrect route\n");
     }
   }
+  /* display waiting to start */
   if (count_by_status(event, NOT_STARTED) > 0) {
     printf("\n\tWaiting to start:\n");
     for (; i < Vector_size(event->entrants); i++) {
       Vector_get(event->entrants, i, &entrant);
+
       printf("\t\t%3d: %-50s\n", entrant->id, entrant->name);
       printf("\t\t\tCourse: %c\n", entrant->course->id);
     }
   }
 }
 
+/*
+ * update an entrant manually
+ */
 void update_manual(Event* event) {
   char* line;
   char type;
@@ -238,12 +285,18 @@ void update_manual(Event* event) {
   time = str_to_time(line);
   free(line);
 
-  update_time(event, time, entrant_id);
-  entrant_update_location(event, type, entrant_id, node_id);
+  update_time(event, time, entrant_id); /* entrant_id refers to an entrant that will
+                                           NOT be updated by this call */
+  entrant_update_location(event, type, entrant_id, node_id); /* they get updated here */
+
+  /* print out the entrant's stats */
   entrant_stats(entrant_from_id(event->entrants, entrant_id), event->time);
   free(time);
 }
 
+/*
+ * update entrants from a file
+ */
 void update_file(Event* event) {
   char* filename = get_filename("Enter checkpoint file: ");
   Vector* lines = read_file(filename);
@@ -282,6 +335,9 @@ void update_file(Event* event) {
   Vector_dispose(lines);
 }
 
+/*
+ * the main method (including program loop)
+ */
 int main(int argc, char* argv[]) {
   Event* event = read_data();
   int running = 1;
